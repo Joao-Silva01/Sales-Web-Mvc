79132c9fa5f6a8ae6dc7207807642ce1242c9dac
João Roberto Silva Felizardo <futurepromise12@gmail.com>
2024-07-11 10:54:54 -0300

    Delete Seller

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -17,5 +17,17 @@ namespace Sales_Web_Mvc.Services
             _context.SaveChanges();
         }
 
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+
     }
 }
